db1e84f0d7c540a6aeb457e41de71006fe145b1c
Dirk Su <dirksu@gmail.com>
2018-12-22 13:59:32 +0800

    add get_stock_code.py

---
+++ get_stock_code.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import requests
+from bs4 import BeautifulSoup
+
+tse_url = "http://isin.twse.com.tw/isin/C_public.jsp?strMode=2"
+otc_url = "http://isin.twse.com.tw/isin/C_public.jsp?strMode=4"
+
+def getStockCode():
+    page = requests.get(tse_url)
+    soup = BeautifulSoup(page.content.decode('cp950'), 'html.parser')
+    trs = soup.find_all('tr')
+    content = []
+    for tr in trs[1:]:
+        if len(tr) < 2:
+            continue
+        if len(tr.contents[4].text) > 2:
+            content.append('%s,%s'%(tr.contents[0].text[:4], tr.contents[0].text[5:]))
+
+    return content
+
+def SaveCSV(data, filename):
+    with open(filename, 'wb') as csvfile:
+        content = '\r\n'.join(row for row in data)
+        csvfile.write(content.encode('utf-8'))
+        print("write %s ok."%(filename))
+
+if __name__ == '__main__':
+    SaveCSV(getStockCode(), 'stock.csv')
