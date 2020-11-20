aa131a4db4759cfd9f8ccfc18e3c6c72fc548256
Dirk Su <dirksu@gmail.com>
2020-11-20 11:51:00 +0800

    Add error handling when parsing CSV data

--- db_bsr.py
+++ db_bsr.py
@@ -18,29 +18,36 @@ def update_stock_bsr(stock):
                        cursorclass=sql.cursors.DictCursor)
     cur = conn.cursor()
     try:
-        cur.execute('''SELECT date FROM `%s` LIMIT 1;''' % stock)
+        cur.execute('''SELECT date FROM `{stock}` LIMIT 1;'''.format(stock=stock))
         conn.commit()
     except (sql.Error) as e:
-        cur.execute('''CREATE TABLE IF NOT EXISTS `%s` ( date DATE NOT NULL, broker VARCHAR(20) NOT NULL, price FLOAT NOT NULL, buy INT NOT NULL, sell INT NOT NULL );''' % stock)
+        cur.execute('''CREATE TABLE IF NOT EXISTS `{stock}` ( date DATE NOT NULL, broker VARCHAR(20) NOT NULL, price FLOAT NOT NULL, buy INT NOT NULL, sell INT NOT NULL );'''.format(stock=stock))
         conn.commit()
 
-    cur.execute('''SELECT * FROM `%s` WHERE date = "%s";''' % (stock, date))
+    cur.execute('''SELECT * FROM `{stock}` WHERE date = "{date}";'''.format(stock=stock, date=date))
 
     #make sure nothing duplicated
     if len(cur.fetchall()) == 0:
-        csvfile = StringIO(get_bsr_csv(stock))
+        data = get_bsr_csv(stock)
+        if data == 'None':
+            return
+        csvfile = StringIO(data)
         rows = csv.reader(csvfile)
-        next(rows)
-        next(rows)
-        next(rows)
-        for row in rows:
-            if len(row) == 0:
-                continue
-            if row[1] != '':
-                cur.execute('''INSERT INTO `%s` (date, broker, price, buy, sell) VALUES ("%s", "%s", %f, %d, %d);''' %
-                        (stock, date, row[1][4:], float(row[2]), int(row[3]), int(row[4])))
-                conn.commit()
-            if row[7] != '':
-                cur.execute('''INSERT INTO `%s`(date, broker, price, buy, sell) VALUES ("%s", "%s", %f, %d, %d);''' %
-                        (stock, date, row[7][4:], float(row[8]), int(row[9]), int(row[10])))
-                conn.commit()
+        try:
+            next(rows)
+            next(rows)
+            next(rows)
+
+            for row in rows:
+                if len(row) == 0:
+                    continue
+                if row[1] != '':
+                    cur.execute('''INSERT INTO `{stock}` (date, broker, price, buy, sell) VALUES ("{date}", "{broker}", {price:f}, {buy:d}, {sell:d});'''.format(stock=stock, date=date, broker=row[1][4:], price=float(row[2]), buy=int(row[3]), sell=int(row[4])))
+                    conn.commit()
+                if row[7] != '':
+                    cur.execute('''INSERT INTO `{stock}` (date, broker, price, buy, sell) VALUES ("{date}", "{broker}", {price:f}, {buy:d}, {sell:d});'''.format(stock=stock, date=date, broker=row[7][4:], price=float(row[8]), buy=int(row[9]), sell=int(row[10])))
+                    conn.commit()
+        except:
+            print('Something wrong with bsr csv data')
+    else:
+        print('find duplicated date {date} in {stock}'.format(date=date, stock=stock))
