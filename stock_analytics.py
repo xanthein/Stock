c6218f845da900ee5c4e9452168f15395f94d4e6
Dirk Su <dirksu@gmail.com>
2020-12-18 17:58:50 +0800

    Add class Analytics

--- stock_analytics.py
+++ stock_analytics.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import sys
 import datetime
 import time
 from scipy import stats
@@ -7,30 +8,25 @@ import numpy
 import twstock
 import csv
 
-def pass_63ma(stock):
-    stock = twstock.Stock(stock, False)
-    today = datetime.datetime.today()
-    before = today - datetime.timedelta(days=100)
-    stock.fetch_from(before.year, before.month)
-    price = list(filter(lambda x: x!=None, stock.price))
-    if len(price) < 63 or stock.price[-1] == None:
-        return False
-    try:
-        ma = stock.moving_average(price, 63)
-        slope, intercept, rvalue, pvalue, stderr = stats.linregress(ma, numpy.arange(0, len(ma)))
-        if stock.price[-1] > ma[-1] and \
-            stock.price[-1] < ma[-1] * 1.06 and \
-            slope > 0:
-            return True
-        else:
-            return False
-    except Exception as e:
-        print(e)
-        return False
+class Analytics(object):
 
-with open('stock.csv', 'r') as csvfile:
-    rows = csv.reader(csvfile)
-    for row in rows:
-        print('%s %s' % (row[0], row[1]))
-        if pass_63ma(row[0]):
-            print("%s" % row[0])
+    def pass_63ma(stock):
+        stock = twstock.Stock(stock, False)
+        today = datetime.datetime.today()
+        before = today - datetime.timedelta(days=100)
+        stock.fetch_from(before.year, before.month)
+        price = list(filter(lambda x: x!=None, stock.price))
+        if len(price) < 63 or stock.price[-1] == None:
+            return False
+        try:
+            ma = stock.moving_average(price, 63)
+            slope, intercept, rvalue, pvalue, stderr = stats.linregress(ma, numpy.arange(0, len(ma)))
+            if stock.price[-1] > ma[-1] and \
+                stock.price[-1] < ma[-1] * 1.06 and \
+                slope > 0:
+                return True, stock
+            else:
+                return False
+        except Exception as e:
+            print(e)
+            return False
