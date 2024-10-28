1bb53e1becc461a1c63fa29de1e267d8d7fdd141
Dirk Su <dirksu@gmail.com>
2024-10-28 22:12:28 +0100

    db.py: add class DataBase

--- stock/db.py
+++ stock/db.py
@@ -4,24 +4,27 @@ import json
 import os
 import sqlite3
 
-def update_stock_report(db_path, date, data):
-    con = sqlite3.connect(db_path)
-    cur = con.cursor()
-    for stock in data:
-        code = stock["Code"]
-        input_date = date.strftime("%Y-%m-%d")
-        open_price = float(stock["OpeningPrice"]) if stock["OpeningPrice"] else 0.0
-        high_price = float(stock["HighestPrice"]) if stock["HighestPrice"] else 0.0
-        low_price = float(stock["LowestPrice"]) if stock["LowestPrice"] else 0.0
-        close_price = float(stock["ClosingPrice"]) if stock["ClosingPrice"] else 0.0
-        volume = int(stock["TradeVolume"]) if stock["TradeVolume"] else 0
-        cur.execute(f"CREATE TABLE IF NOT EXISTS table_{code} (date STRING, open_price REAL, high_price REAL, low_price REAL, close_price REAL, volume INTEGER)")
-        cur.execute(f"SELECT date FROM table_{code} WHERE date = '{input_date}'")
-        entry = cur.fetchone()
-        if entry is None:
-            cur.execute(f"INSERT INTO table_{code} (date, open_price, high_price, low_price, close_price, volume) VALUES ('{input_date}', {open_price}, {high_price}, {low_price}, {close_price}, {volume})")
-    con.commit()
-    con.close()
+class DataBase:
+    def __init__(self, path):
+        self.conn = sqlite3.connect(path)
+        self.cursor = self.conn.cursor()
+    def __del__(self):
+        self.conn.commit()
+        self.conn.close()
+    def update_stock_report(self, date, data):
+        for stock in data:
+            code = stock["Code"]
+            input_date = date.strftime("%Y-%m-%d")
+            open_price = float(stock["OpeningPrice"]) if stock["OpeningPrice"] else 0.0
+            high_price = float(stock["HighestPrice"]) if stock["HighestPrice"] else 0.0
+            low_price = float(stock["LowestPrice"]) if stock["LowestPrice"] else 0.0
+            close_price = float(stock["ClosingPrice"]) if stock["ClosingPrice"] else 0.0
+            volume = int(stock["TradeVolume"]) if stock["TradeVolume"] else 0
+            self.cursor.execute(f"CREATE TABLE IF NOT EXISTS table_{code} (date STRING, open_price REAL, high_price REAL, low_price REAL, close_price REAL, volume INTEGER)")
+            self.cursor.execute(f"SELECT date FROM table_{code} WHERE date = '{input_date}'")
+            entry = self.cursor.fetchone()
+            if entry is None:
+                self.cursor.execute(f"INSERT INTO table_{code} (date, open_price, high_price, low_price, close_price, volume) VALUES ('{input_date}', {open_price}, {high_price}, {low_price}, {close_price}, {volume})")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -35,6 +38,7 @@ if __name__ == '__main__':
     parser.add_argument("db", type=str)
 
     args = parser.parse_args()
+    db = DataBase(args.db)
 
     if args.subcommand == "add":
         try:
@@ -45,6 +49,6 @@ if __name__ == '__main__':
                 else:
                     with open(args.trading, "r") as fd:
                         data = json.load(fd)
-                        update_stock_report(args.db, date, data)
+                        db.update_stock_report(date, data)
         except Exception as error:
             print(error)
