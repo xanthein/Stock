edab61a9ebfcfeef063ad5cf8f2846bdbfaf6c02
Dirk Su <dirksu@gmail.com>
2025-02-11 16:16:15 +0800

    Try to filter ma5 with positive slope

--- stock/analytics.py
+++ stock/analytics.py
@@ -2,6 +2,7 @@
 
 from scipy import stats
 import pandas as pd
+import numpy
 
 # pandas data format (open, high, low, close, volume) with date as index
 
@@ -11,8 +12,12 @@ class Analytics():
         self.data["63ma"] = self.data["close"].rolling(window=63).mean()
         self.data["5ma"] = self.data["close"].rolling(window=5).mean()
     def pass_63ma(self):
+        ma5_drop = self.data["5ma"].dropna()
+        ma5_cal = stats.linregress(numpy.arange(0, len(ma5_drop)), ma5_drop)
         if self.data["5ma"].values[-1] > self.data["63ma"].values[-1] and \
-           self.data["5ma"].values[-1] < self.data["63ma"].values[-1] * 1.06:
-            return True
+           ma5_cal.slope > 0:
+            for i in self.data["5ma"].values[-5:]:
+                if i < self.data["63ma"].values[-1]:
+                    return True
         return False
 
